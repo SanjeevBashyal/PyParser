ac8bd5d8409d3527cacae3cb4fd1b33cf024edf6
Sanjeev Bashyal <sanjeev.bashyal01@gmail.com>
2022-11-01 19:23:06 +0545

    7/10

--- MySqlVisitor.py
+++ MySqlVisitor.py
@@ -1,4 +1,3 @@
-import imp
 from antlr4 import *
 from SqlVisitor import SqlVisitor
 from SqlParser import SqlParser
@@ -7,13 +6,48 @@ class MySqlVisitor(SqlVisitor):
     def __init__(self):
         self.stack = []
         self.tables = {}
+        self.columns = {}
+        self.data = {}
+        
     def visitProg(self, ctx:SqlParser.ProgContext):
         return self.visit(ctx.expr()) # Just visit the self expression
+
     def visitCreate_stmt(self, ctx:SqlParser.Create_stmtContext):
-        print(ctx.getText())
-        print(ctx.ID())
+        table_name=str(ctx.ID())
         if ctx.ID() in self.tables:
             raise ValueError(f"table {ctx.ID()} already exists")
         else:
-            self.tables.update(ctx.ID():str(ctx.ID()))
-        # Save the table definition in the dictionary
+            self.tables[ctx.ID()]=table_name
+            self.columns[table_name]=[]
+            self.data[table_name]=[]
+        return self.visitChildren(ctx)
+
+    def visitColumn_spec(self, ctx:SqlParser.Column_specContext):
+        table_name=str(ctx.parentCtx.parentCtx.ID())
+        column_name=str(ctx.ID())
+        column_data=[column_name,ctx.column_type().getText()]
+        self.columns[table_name].append(column_data)
+        return self.visitChildren(ctx)
+
+    def visitInsert_stmt(self, ctx:SqlParser.Insert_stmtContext):
+        table_name=str(ctx.ID())
+        val=ctx.VAL()
+        count=len(val)
+        if count != len(self.columns[table_name]):
+            raise ValueError(f"Column count and data count mismatch in table {table_name}")
+        else:
+            ls=[]
+            for i in range(count):
+                data_element=val[i].getText()
+                if self.columns[table_name][i][1]=='string':
+                    if data_element[0]!='"':
+                        raise ValueError(f"Column {self.columns[table_name][i][1]} of Table {table_name} requires string data type")
+                    else:
+                        ls.append(data_element[1:-1])
+                else:
+                    if data_element[0]=='"':
+                        raise ValueError(f"Column {self.columns[table_name][i][1]} of Table {table_name} requires string int type")
+                    else:
+                        ls.append(data_element)
+        self.data[table_name].append(ls)
+        return self.visitChildren(ctx)    
