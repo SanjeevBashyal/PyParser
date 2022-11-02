9c735bc5458123b069defc332cd91bb8feee0b83
Sanjeev Bashyal <sanjeev.bashyal01@gmail.com>
2022-11-02 06:59:41 +0545

    9/10

--- MySqlVisitor.py
+++ MySqlVisitor.py
@@ -31,6 +31,8 @@ class MySqlVisitor(SqlVisitor):
 
     def visitInsert_stmt(self, ctx:SqlParser.Insert_stmtContext):
         table_name=str(ctx.ID())
+        if not table_name in self.columns:
+            raise ValueError(f"Table {table_name} is not found")
         val=ctx.VAL()
         count=len(val)
         if count != len(self.columns[table_name]):
@@ -51,3 +53,49 @@ class MySqlVisitor(SqlVisitor):
                         ls.append(data_element)
         self.data[table_name].append(ls)
         return self.visitChildren(ctx)    
+    
+    def visitSelect_stmt(self, ctx:SqlParser.Select_stmtContext):
+        ids=ctx.ID()
+        table_name=ids[-1].getText()
+        total_number_of_columns=len(self.columns[table_name])
+        all_columns=list(range(total_number_of_columns))
+        columns_required=[]
+        rows_required=list(range(len(self.data[table_name])))
+        if ctx.getChild(1).getText()=='*':
+            columns_required=all_columns
+        else:
+            check_list_of_columns=all_columns
+            for i in range(len(ids)-1):
+                column_name=ids[i].getText()
+                for j in check_list_of_columns:
+                    if column_name==self.columns[table_name][j][0]:
+                        columns_required.append(j)
+                        check_list_of_columns.remove(j)
+
+        if len(ctx.cond())>0:
+            rows_required=self.visitConditions(ctx.cond(), table_name, rows_required)
+
+        data=self.fetch(table_name, columns_required, rows_required)
+        self.show(data)
+
+        return self.visitChildren(ctx)
+
+    def visitConditions(self, condContextList, table_name, rows_required):
+        pass
+
+    def visitCond(self, ctx:SqlParser.CondContext):
+        return self.visitChildren(ctx)
+
+    def fetch(self, table_name, columns_required, rows_required):
+        column_names=[self.columns[table_name][j][0] for j in columns_required]
+        data=[[self.data[table_name][i][j] for j in columns_required] for i in rows_required]
+        data.insert(0,column_names)
+        return data
+
+    def show(self, data):
+        print()
+        for data_row in data:
+            for data_element in data_row:
+                print(data_element,'\t\t', end="", flush=True)               
+            print()
+        print()
